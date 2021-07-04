129261da1ac588cf3c4c292d4cf52dd30a9bc702
Adonis <adonis.rumbwere@cassavasmartech.co.zw>
2021-07-04 13:21:36 +0200

    added Api Request swift file

--- CodeChallengeNASA/WebService/ApiService.swift
+++ CodeChallengeNASA/WebService/ApiService.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+class APIService {
+    
+    private var dataTask: URLSessionDataTask?
+    
+    func getImagesData(completion: @escaping(Result<CollectionData, Error>) -> Void) {
+        
+        let nasaImagesUrl = "https://images-api.nasa.gov/search?q=%22%22"
+        
+        guard let url = URL(string: nasaImagesUrl) else { return }
+        
+        //Create URL Session - work on the background
+        
+        dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            //Handle Error
+            
+            if let error = error {
+                completion(.failure(error))
+                print("DataTask error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                //Handle Empty Response
+                return
+            }
+            
+            print("Response status code: \(response.statusCode)")
+            
+            guard let data = data else {
+                //Handle Empty Data
+                print("Empty Data")
+                return
+            }
+            
+            do {
+                //Parse the data
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(CollectionData.self, from: data)
+                
+                //Back to the mnain thread
+                DispatchQueue.main.async {
+                    completion(.success(jsonData))
+                }
+            } catch let error {
+                completion(.failure(error))
+            }
+        }
+        dataTask?.resume()
+        
+    }
+      
+}
